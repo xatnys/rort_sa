3918025f8c26275a5754bbb929b3a6fe942cc4c1
snera <xatnys@gmail.com>
2013-03-31 13:36:12 -0400

    Replace jquery bind() with on() for micropost char counter

--- app/assets/javascripts/static_pages/home.js
+++ app/assets/javascripts/static_pages/home.js
@@ -1,16 +1,13 @@
-$(function() {
-	$("#micropost_content").bind('keyup', function() {
-		var thres = 140-$(this).val().length
-		if ( thres == 140 ) {
-			$('#chars').text("");
-		} else {
-			$('#chars').text(thres + " characters remaining.");
-		}
-		if ( thres <= 0 ) {
-			$('#chars').addClass("maxlimit");
-		} else {
-			$('#chars').removeClass("maxlimit");
-		}
-		return false;
-	})
-})
+$(document).on('keyup', '#micropost_content', function(){ 
+	var thres = 140-$(this).val().length;
+	if (thres <= 0 ) { 
+		$('#chars').addClass("maxlimit"); 
+	} else {
+		$('#chars').removeClass("maxlimit");
+	}
+	if (thres == 140) {
+		$('#chars').text("");
+	} else {
+		$('#chars').text(thres + " characters remaining.");
+	}
+});
